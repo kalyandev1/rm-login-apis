58dbfd88ddd7765c7aa05b79f20a84e2d87721a3
kalyandev1 <kalyan@metalok.io>
2024-04-08 19:52:04 +0530

    changes added

--- index.js
+++ index.js
@@ -2,27 +2,27 @@
 const express = require("express");
 const app = express();
 const router = require('express').Router()
-const http = require('http')
+// const http = require('http')
+const https = require('https')
+
 const server = require('./src/frameworks_drivers/server/server')
 const sequelize = require('./src/frameworks_drivers/database/sequelize')
 const cors = require("cors");
-
+let fs = require('fs');
 const port = process.env.PORT || 3001
 
-let myserver = http.Server(server);
+
+
+// let myserver = https.Server(server);
 
 app.use(cors('*'));
-app.get('/',(req,res)=>{
-    res.send(`<html>
-    <body>
-        <h1 style="color:blue;text-align: center;margin-top: 100px;"> [Version : This is AMAZING!!! Like & Subscribe!</h1>
-        <div style="position: fixed;top: 50%;left: 50%;transform: translate(-50%, -50%)">
-        <h1>hello world</h1>
-        </div>
-    </body>
-   </html>`);
-})
 
+var options = {
+    key: fs.readFileSync('./certificates/server.key'),
+    cert: fs.readFileSync('./certificates/servercrt.crt'),
+};
+
+let myserver = https.Server(options, server);
 try {
     ("step1")
    
