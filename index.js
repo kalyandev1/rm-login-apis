0a454c46ac0414cc004a5f6a0ebc794e18ef6b8b
kalyandev1 <kalyan@metalok.io>
2024-04-13 14:28:06 +0530

    crt key files uploaded

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 const express = require("express");
 const app = express();
 const router = require('express').Router()
-const http = require('http')
-// const https = require('https')
+// const http = require('http')
+const https = require('https')
 
 const server = require('./src/frameworks_drivers/server/server')
 const sequelize = require('./src/frameworks_drivers/database/sequelize')
@@ -11,18 +11,18 @@ const cors = require("cors");
 let fs = require('fs');
 const port = process.env.PORT || 3001
 
-
-
-let myserver = http.Server(server);
+// let myserver = http.Server(server);
 
 app.use(cors('*'));
 
 var options = {
-    key: fs.readFileSync('./certificates/server.key'),
-    cert: fs.readFileSync('./certificates/servercrt.crt'),
+    key: fs.readFileSync('./certificates/privatekey.key'),
+    cert: fs.readFileSync('./certificates/certificate.crt.crt'),
 };
-
-// let myserver = https.Server(options, server);
+  
+    // key: fs.readFileSync('./certificates/server.key'),
+    // cert: fs.readFileSync('./certificates/servercrt.crt'),  
+let myserver = https.Server(options, server);
 try {
     ("step1")
    
@@ -30,12 +30,14 @@ try {
    
 } catch (error) {
    
-}
+} 
 try {
     console.log("server running on ",port)
-
+  
     myserver.listen(port)
 } catch (error) {
     console.log('failed to start the server')
-}
+} 
+  
+
 
